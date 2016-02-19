8cdb4e1fb8904ac50f9cd8232544da93d6c9f823
Kathryn Hodge <kahodge@vassar.edu>
2016-02-19 12:47:31 -0500

    Reading and writing to files

--- Python Basics/readwrite.py
+++ Python Basics/readwrite.py
@@ -1,1 +1,17 @@
-# How to 
+# How to read/write from files in python
+
+from sys import argv
+
+# open(fileName, mode)
+f = open("read.txt", "w+")
+
+# Read the contents of the file
+print(f.read())
+
+# Write to a file 
+f.write("Oh hai there\n")
+
+# Read again
+print(f.read())
+
+f.close()
