8f08114834c2a3f64ef72c2b1036d782a2c8ea75
Kathryn Hodge <kahodge@vassar.edu>
2016-03-01 17:04:12 -0500

    Added Detail Page

--- Django-Tutorials/blog/urls.py
+++ Django-Tutorials/blog/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
 	# Assigning a view to a URL
 	url(r'^$', views.post_list, name='post_list'),
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
 
 # Create virtual environment: source myvenv/bin/activate
