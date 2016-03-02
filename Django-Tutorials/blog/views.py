b6c8bb848ff4073a51bd8b2f70babff381828513
Kathryn Hodge <kahodge@vassar.edu>
2016-03-02 12:55:47 -0500

    Finished first Django Girls tutorial

--- Django-Tutorials/blog/views.py
+++ Django-Tutorials/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from .models import Post
 from .forms import PostForm
+
 
 # Create your views here.
 
@@ -15,5 +17,28 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    form = PostForm()
+    if (request.method == "POST"):
+    	form = PostForm(request.POST)
+    	if (form.is_valid()):
+    		post = form.save(commit=False)
+    		post.author = request.user
+    		post.published_date = timezone.now()
+    		post.save()
+    		return redirect('post_detail', pk=post.pk)
+    else:
+    	form = PostForm()
+    return render(request, 'blog/post_new.html', {'form':form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
     return render(request, 'blog/post_new.html', {'form': form})
