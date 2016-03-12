9e719e6ea20f65d059ab51993c735ac7ace3254d
Kathryn Hodge <kahodge@vassar.edu>
2016-03-12 17:32:17 -0500

    Having problems running --> getting a TypeError everytime I vote

--- Django-Documentation-Tutorial/mysite/polls/views.py
+++ Django-Documentation-Tutorial/mysite/polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.http import Http404
 from django.template import loader
-from .models import Question
+from .models import Question, Choice
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 
@@ -24,8 +25,28 @@ def detail(request, question_id):
 	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk = question_id)
+	return HttpResponse(request, 'polls/results.html', {'question' : question})
 
-def vote(requestion, question_id):
-	return HttpResponse("You are voting on question %s." % question_id)
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
+
+
+
+
