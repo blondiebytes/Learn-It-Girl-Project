0a810f279dd0bedb5851be402b885834e7288005
Kathryn Hodge <kahodge@vassar.edu>
2016-03-13 18:29:03 -0400

    Finished Part 5 of Django-Documentation-Tutorial

--- Django-Documentation-Tutorial/mysite/polls/views.py
+++ Django-Documentation-Tutorial/mysite/polls/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from .models import Question, Choice
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 
@@ -13,8 +14,10 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_question_list'
 
 	def get_queryset(self):
-		"""Return the last five published questions."""
-		return Question.objects.order_by('-pub_date')[:5]
+		"""Return the last five published questions (not including
+			those set to be published in the future."""
+		#return Question.objects.order_by('-pub_date')[:5]
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 #def index(request):
 	#latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -29,6 +32,10 @@ class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
 
+	def get_queryset(self):
+		""" Excludes any questions that aren't published yet"""
+		return Question.objects.filter(pub_date__lte=timezone.now())
+
 #def detail(request, question_id):
 	#try:
 	#	question = Question.objects.get(pk=question_id)
@@ -40,6 +47,10 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
 	model = Question
 	template_name = 'polls/results.html'
+
+	def get_queryset(self):
+		""" Excludes any questions that aren't published yet"""
+		return Question.objects.filter(pub_date__lte=timezone.now())
 
 #def results(request, question_id):
 	#question = get_object_or_404(Question, pk = question_id)
